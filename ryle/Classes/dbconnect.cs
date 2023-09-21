e3337b9d2dab99fbc11ffdcfb2950e37e85983fd
eroshevskiy <eroshevskiy.oleg@mail.ru>
2023-09-21 22:42:48 +0300

    ryle

---
+++ ryle/Classes/dbconnect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ryle.Classes
+{
+    class dbconnect
+    {
+        public static Models.ryleEntities1 modeldb;
+    }
+}
---
+++ ryle/Pages/Autor.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ryle.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Autor.xaml
+    /// </summary>
+    public partial class Autor : Window
+    {
+        public Autor()
+        {
+            InitializeComponent();
+
+            Classes.dbconnect.modeldb = new Models.ryleEntities1();
+        }
+
+        private void Enter(object sender, RoutedEventArgs e)
+        {
+
+
+            var userobj = Classes.dbconnect.modeldb.users.FirstOrDefault(x =>
+            login.Text == x.login && pass.Password == x.pasword);
+
+            
+
+            Admin objAdmin = new Admin();
+            this.Visibility = Visibility.Hidden;
+            objAdmin.Show();
+            
+
+            
+        }
+    }
+}
